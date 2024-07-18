4d62430bd55cd4b0bffd35646f9ca0c4dcbc2910
HolographicX <divyamseksaria09@gmail.com>
2024-07-18 15:34:04 +0530

    ags

--- modules/nixos/desktop/gnome/default.nix
+++ modules/nixos/desktop/gnome/default.nix
@@ -143,7 +143,7 @@ in {
 
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
         binding = "<Super>Return";
-        command = "kgx";
+        command = "alacritty";
         name = "term";
       };
 
