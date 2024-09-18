c18274a67a368596a781728b4fc332fc9f80b329
HolographicX <divyamseksaria09@gmail.com>
2024-09-18 13:17:34 -0400

    483   2024-09-18 13:17:31   (current)

--- modules/nixos/apps/lutris/default.nix
+++ modules/nixos/apps/lutris/default.nix
@@ -16,6 +16,7 @@ in {
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       lutris
+      wineWowPackages.staging
       wineWowPackages.waylandFull
       winetricks
     ];
