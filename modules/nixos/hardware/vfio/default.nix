8976928ea305caab970d048aae699e870d99a0eb
HolographicX <divyamseksaria09@gmail.com>
2024-10-19 11:58:14 -0400

    494   2024-10-19 11:58:10   (current)

--- modules/nixos/hardware/vfio/default.nix
+++ modules/nixos/hardware/vfio/default.nix
@@ -19,6 +19,22 @@ in
   };
 
   config = mkIf cfg.enable {
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [(pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd];
+        };
+      };
+    };
+  
     boot = {
       initrd.kernelModules = [
         "vfio_pci"
