5520e0d66dbd4eda548a3048fa94e37a0756a916
HolographicX <divyamseksaria09@gmail.com>
2025-06-17 02:28:06 -0400

    596   2025-06-17 02:28:04   (current)

--- modules/nixos/system/time/default.nix
+++ modules/nixos/system/time/default.nix
@@ -13,5 +13,8 @@ in {
       mkBoolOpt false "Whether or not to configure timezone information.";
   };
 
-  config = mkIf cfg.enable {services.automatic-timezoned.enable = true;};
+  config = mkIf cfg.enable {
+    services.geoclue2.geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
+    services.automatic-timezoned.enable = true;
+  };
 }
