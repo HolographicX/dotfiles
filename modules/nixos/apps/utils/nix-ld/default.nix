33921cd509ed4a657fd82988599ff743a8602de7
HolographicX <divyamseksaria09@gmail.com>
2025-07-02 13:27:01 +0530

    608   2025-07-02 13:26:57   (current)

--- modules/nixos/apps/utils/nix-ld/default.nix
+++ modules/nixos/apps/utils/nix-ld/default.nix
@@ -17,11 +17,7 @@ in {
     programs.nix-ld.enable = true;
 
     programs.nix-ld.libraries = with pkgs; [
-
-      # Add any missing dynamic libraries for unpackaged programs
-
-      # here, NOT in environment.systemPackages
-
+      cudaPackages.libcublas
     ];  
   };
 }
