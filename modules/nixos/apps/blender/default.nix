c120010f99dd20933255d1c095de958bc6b0fbba
HolographicX <divyamseksaria09@gmail.com>
2025-07-27 19:35:45 +0530

    621   2025-07-27 19:35:36   (current)

--- modules/nixos/apps/blender/default.nix
+++ modules/nixos/apps/blender/default.nix
@@ -10,10 +10,10 @@ with lib.custom; let
   cfg = config.apps.blender;
 in {
   options.apps.blender = with types; {
-    enable = mkBoolOpt false "Enable or disable alacritty";
+    enable = mkBoolOpt false "Enable or disable blender";
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ blender ];
+    environment.systemPackages = with pkgs; [ blender_4_5 ]; # from blender-bin flake
   };
 }
