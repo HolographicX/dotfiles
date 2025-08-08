716022d02a6f507ffd6eba2d876bc7e18bcf0221
HolographicX <divyamseksaria09@gmail.com>
2025-08-08 20:16:51 +0530

    630   2025-08-08 20:16:48   (current)

--- modules/nixos/hardware/networking/default.nix
+++ modules/nixos/hardware/networking/default.nix
@@ -25,13 +25,13 @@ in {
           workstation = true;
         };
     };
-    networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
 
     services.resolved = {
       enable = true;
       dnssec = "true";
       domains = [ "~." ];
-      fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+      fallbackDns = [ "1.1.1.1" "8.8.8.8" ];
       dnsovertls = "true";
     };
   };
